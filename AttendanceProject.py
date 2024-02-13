e3bbe4953b3286d60dff6b0ccbf60d4eea4d3e67
unknown <akir996@aucklanduni.ac.nz>
2024-02-14 11:22:45 +1300

    feature/added some ui using customtkinter

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -1,8 +1,43 @@
-import cv2
-import numpy as np
-import face_recognition
-import os
 from datetime import datetime
+import os
+import customtkinter
+from tkinter import *
+import cv2 
+from PIL import Image, ImageTk 
+import face_recognition
+import numpy as np
+  
+# Define a video capture object 
+vid = cv2.VideoCapture(0) 
+  
+# Declare the width and height in variables 
+width, height = 800, 600
+  
+# Set the width and height 
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, width) 
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height) 
+  
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+
+
+# set app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Attendance Portal")
+
+# add some UI elements
+title = customtkinter.CTkLabel(app,text="Please Look Into The Camera")
+title.pack(padx=10, pady=10)
+  
+# Bind the app with Escape keyboard to 
+# quit app whenever pressed 
+app.bind('<Escape>', lambda e: app.quit()) 
+  
+# Create a label and display it on app 
+label_widget = Label(app) 
+label_widget.pack() 
 
 # create a list of filenames in the face image folder
 path = 'attendance-system/Face_Images'
@@ -37,21 +72,26 @@ def markAttendance(name):
             now = datetime.now()
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
-            
+
 
 # generate a list of encodings for the images
 encodeList =  findEncodings(images)
-
-# turn on webcam video capture
-cap = cv2.VideoCapture(0)
-
-# read each frame of the webcam input, encode it and compare it to the list of encoded images on file
-while True:
-    success, img = cap.read()
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-
+  
+# Create a function to open camera and 
+# display it in the label_widget on app 
+  
+  
+def open_camera(): 
+  
+    # Capture the video frame by frame 
+    _, frame = vid.read() 
+  
+    # Convert image from one color space to other 
+    
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     facesCurFrame = face_recognition.face_locations(img)
     encodingsCurFrame = face_recognition.face_encodings(img,facesCurFrame)
+
 
 # compare the current encoding with those in the list and find the closest match
     for encodeFace, faceLoc in zip(encodingsCurFrame, facesCurFrame):
@@ -63,11 +103,50 @@ while True:
         if matches[matchIndex]:
            name = classNames[matchIndex].upper()
            y1,x2,y2,x1 = faceLoc
-           cv2.rectangle(img,(x1,y1),(x2,y2), (0,255,0),2)
-           cv2.rectangle(img,(x1,y2-35),(x2,y2), (0,255,0), cv2.FILLED)
-           cv2.putText(img, name,(x1+6, y2-6), cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+
+           # Redraw the canvas to update the rectangles
+          
+           cv2.rectangle( frame,(x1,y1),(x2,y2), (0,255,0),2)
+           cv2.rectangle(frame,(x1,y2-35),(x2,y2), (0,255,0), cv2.FILLED)
+           cv2.putText(frame, name,(x1+6, y2-6), cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
            markAttendance(name)
 
-#show the webcam on-screen
-    cv2.imshow('WebCam',img)
-    cv2.waitKey(1)
+    # Capture the latest frame and transform to image 
+    opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA) 
+    captured_image = Image.fromarray(opencv_image) 
+  
+    # Convert captured image to photoimage 
+    photo_image = ImageTk.PhotoImage(image=captured_image) 
+  
+    # Displaying photoimage in the label 
+    label_widget.photo_image = photo_image 
+  
+    # Configure image in the label 
+    label_widget.configure(image=photo_image) 
+  
+    # Repeat the same process after every 10 seconds 
+    label_widget.after(10, open_camera) 
+  
+  
+# Create a button to open the camera in GUI app 
+# button1 = Button(app, text="Open Camera", command=open_camera) 
+# button1.pack() 
+    
+open_camera()
+# Create an infinite loop for displaying app on screen 
+app.mainloop() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
